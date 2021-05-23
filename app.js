77e976e4bba7446632e513886ed52a24077c1bad
it-dreams <rahulraj.holi888@gmail.com>
2021-05-23 18:53:57 +0530

    Create DB from code

--- app.js
+++ app.js
@@ -20,6 +20,15 @@ db.connect((err) => {
     console.log('MySQL connected...');
 })
 
+// Create DB
+app.get('/createdb', (req, res) => {
+    let sql = 'CREATE DATABASE education_blog';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Database created...');
+    })
+})
 
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
