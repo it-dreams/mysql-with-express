6075dac06da0174d46cc49723018783f4d3e9d2e
it-dreams <rahulraj.holi888@gmail.com>
2021-05-23 18:54:40 +0530

    Create table from code

--- app.js
+++ app.js
@@ -30,6 +30,16 @@ app.get('/createdb', (req, res) => {
     })
 })
 
+// Create Table
+app.get('/createpoststable', (req, res) => {
+    let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY(id))';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Posts table created...');
+    });
+});
+
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
 })
